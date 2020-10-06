478e30e6cf48527cb9990f29b5bbcfcf0d22df9d
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-10-06 17:18:57 +0530

    game functionalities achieved

--- app.js
+++ app.js
@@ -20,30 +20,50 @@ document.getElementById("current-1").textContent = "0";
 document.getElementById("score-0").textContent = "0";
 document.getElementById("score-1").textContent = "0";
 
+function swapPlayer() {
+    activePlayer === 1 ? activePlayer = 0 : activePlayer = 1;
+    roundScore = 0;
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    document.querySelector(".dice").style.display = "none";
+    console.log("I've reached line 31");
+
+}
+
 document.querySelector(".btn-roll").addEventListener("click", function() {
     // random number assignment to dice
     var dice = Math.ceil(Math.random() * 6);
     // display this number on the dice
     var diceDOM = document.querySelector('.dice');
-    diceDOM.src = "dice-" + dice + ".png";
     diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
+
     // display the obtained number on the current score as well
-    document.getElementById("current-" + activePlayer).textContent = dice;
     if (dice !== 1) {
         // Add score in the pocket of the currently active player
         roundScore += dice;
-        document.getElementById("score-" + activePlayer).textContent = roundScore;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    } else {
+        // swap the palyer and reset thescore of the by far currrently active player as 0 again
+        swapPlayer();
+    }
+
+});
+
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+    score[activePlayer] += roundScore;
+    document.querySelector("#score-" + activePlayer).textContent = score[activePlayer];
+    if (score[activePlayer] >= 20) {
+        document.getElementById("name-" + activePlayer).textContent = "WINNER!";
+        document.querySelector(".dice").style.display = "none";
+        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
 
     } else {
-        // swap the palyer and reset the scores of the by far currrently active player as 0 again
-        roundScore = 0;
-        activePlayer === 1 ? activePlayer = 0 : activePlayer = 1;
-        document.getElementById("current-0").textContent = "0";
-        document.getElementById("current-1").textContent = "0";
-        document.querySelector(".player-0-panel").classList.toggle("active");
-        document.querySelector(".player-1-panel").classList.toggle("active");
-        document.querySelector(".dice").style.display = "none";
-
+        swapPlayer();
     }
 
 });
