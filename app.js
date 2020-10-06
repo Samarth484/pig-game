44533add3c69a9ae2f31860fbe605a042b202515
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-10-06 17:35:17 +0530

    Finally its done!

--- app.js
+++ app.js
@@ -10,15 +10,8 @@ GAME RULES:
 */
 
 var score, activePlayer, roundScore;
-score = [0, 0];
-activePlayer = 0;
-roundScore = 0;
 
-document.querySelector(".dice").style.display = "none";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
+init();
 
 function swapPlayer() {
     activePlayer === 1 ? activePlayer = 0 : activePlayer = 1;
@@ -61,9 +54,35 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
         document.querySelector(".dice").style.display = "none";
         document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
         document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+        document.querySelector(".btn-roll").style.display = "none";
+        document.querySelector(".btn-hold").style.display = "none";
 
     } else {
         swapPlayer();
     }
 
 });
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+function init() {
+    score = [0, 0];
+    activePlayer = 0;
+    roundScore = 0;
+
+    document.querySelector(".dice").style.display = "none";
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+    document.getElementById("score-0").textContent = "0";
+    document.getElementById("score-1").textContent = "0";
+    document.getElementById("name-0").textContent = "Player 1";
+    document.getElementById("name-1").textContent = "Player 2";
+    document.querySelector(".btn-roll").style.display = "block";
+    document.querySelector(".btn-hold").style.display = "block";
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("winner");
+    document.querySelector(".player-0-panel").classList.add("active");
+    document.querySelector(".player-1-panel").classList.remove("active");
+
+}
